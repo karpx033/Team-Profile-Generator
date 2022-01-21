403e6e28e606ce2c1302da254cbf304376d19e80
Louis <karpx033@umn.edu>
2022-01-20 20:24:55 -0600

    changed the string that linked the css file. All logic seems to be funcioning correctly

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const Choices = require('inquirer/lib/objects/choices');
+const Intern = require("./lib/Intern");
+const Engineer = require('./lib/Engineer');
 const employee = require('./lib/Employee.js');
-const engineer = require('./lib/Engineer.js');
-const intern = require('./lib/Intern.js');
 const Manager = require('./lib/Manager.js');
 const genTeam = require('./src/page-template.js');
-const peeps = [];
+// const styles = require('./dist/style.css');
+
+const team = [];
 
 const questions = [
     'What is the name of the team manager?',
@@ -16,14 +18,133 @@ const questions = [
     'Would you like to add an engineer or intern, or are you finished?'
 ];
 
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
+const internquestions = [
+    'What is the name of the intern?',
+    'What is their employee ID?',
+    'What is their email address?',
+    'What is their school?',
+    'Would you like to add another engineer or intern, or are you finished?'
+];
+
+const engquestions = [
+    'What is the name of the engineer?',
+    'What is their employee ID?',
+    'What is their email address?',
+    'What is their GitHub username?',
+    'Would you like to add another engineer or intern, or are you finished?'
+];
+
+function writeToFile(fileName, print) {
+    fs.writeFile(`${fileName}.html`, print, (err) => {
         if (err) {
             console.log(err);
         }
         console.log("Successful");
     })
 }
+
+function getIntern() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: internquestions[0],
+            name: "internname"
+           },
+           {
+            type: "input",
+            message: internquestions[1],
+            name: "internid"
+           },
+           {
+            type: "input",
+            message: internquestions[2],
+            name: "internaddress"
+           },
+           {
+            type: "input",
+            message: internquestions[3],
+            name: "internschool"
+           },
+           {
+            type: "list",
+            message: internquestions[4],
+            name: "internaddition",
+            choices: ["Engineer", "Intern", "No, generate my team"]
+           },
+    ])
+    .then((answers) => {
+        var {internname, internid, internaddress, internschool, internaddition} = answers;
+        const intern = new Intern(internname, internid, internaddress, internschool);
+        team.push(intern);
+        if (answers.internaddition==="Engineer"){
+            getEngineer();
+        } if (answers.internaddition==="Intern") {
+            getIntern();
+        } else {
+            var fileName = 'Team Contacts';
+            var data = team;
+            var print = genTeam(data);
+            writeToFile(fileName, print);
+        }
+    
+    
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
+}
+
+function getEngineer () {
+    inquirer.prompt ([
+        {
+            type: "input",
+            message: engquestions[0],
+            name: "internname"
+           },
+           {
+            type: "input",
+            message: internquestions[1],
+            name: "engid"
+           },
+           {
+            type: "input",
+            message: engquestions[2],
+            name: "engaddress"
+           },
+           {
+            type: "input",
+            message: engquestions[3],
+            name: "enggithub"
+           },
+           {
+            type: "list",
+            message: engquestions[4],
+            name: "engaddition",
+            choices: ["Engineer", "Intern", "No, generate my team"]
+           },
+    ])
+    .then ((answers) =>{
+        var {engname, engid, engaddress, enggithub, engaddition} = answers;
+        const engineer = new Engineer(engname, engid, engaddress, enggithub);
+        team.push(engineer);
+        if (answers.engaddition==="Engineer"){
+            getEngineer();
+        } if (answers.engaddition==="Intern") {
+            getIntern();
+        } else {
+            var fileName = 'Team Contacts';
+            var data = team;
+            var print = genTeam(data);
+            writeToFile(fileName, print);
+        }
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
+
+
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt([
@@ -57,17 +178,16 @@ function init() {
    .then((answers) => {
         var {name, id, address, officenumber, addition} = answers;
         const leader = new Manager(name, id, address, officenumber);
-        peeps.push(leader);
+        team.push(leader);
         if (answers.addition==="Intern") {
             getIntern();
         } if (answers.addition==="Engineer") {
             getEngineer();
         } else {
-        var fileName = 'Team Contacts';
-        var data = peeps;
-        genTeam(data);
-        var data = genTeam(data);
-        writeToFile(fileName, data);
+            var fileName = 'Team Contacts';
+            var data = team;
+            var print = genTeam(data);
+            writeToFile(fileName, print);
         }
     })
     .catch(err => {
